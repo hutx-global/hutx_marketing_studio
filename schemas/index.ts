c8da6b3de49e0d01be4a3849955ade7e52d94f21
hutx-global <hutmarketingbusiness@gmail.com>
2026-03-22 16:02:40 -0400

    Updated index.ts

---
+++ schemas/index.ts
@@ -0,0 +1,13 @@
+import post from './post'
+import testimonial from './testimonial'
+import caseStudy from './caseStudy'
+import faq from './faq'
+import pricing from './pricing'
+
+export const schemaTypes = [
+  post,
+  testimonial,
+  caseStudy,
+  faq,
+  pricing,
+]
