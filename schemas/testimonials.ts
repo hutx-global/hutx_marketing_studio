5b1264765dcc93d7bcbd76c0fe45aa1e30b5db41
hutx-global <hutmarketingbusiness@gmail.com>
2026-03-22 16:04:12 -0400

    Updated testimonials.ts

---
+++ schemas/testimonials.ts
@@ -0,0 +1,11 @@
+export default {
+  name: 'testimonial',
+  title: 'Testimonial',
+  type: 'document',
+  fields: [
+    { name: 'name', type: 'string' },
+    { name: 'company', type: 'string' },
+    { name: 'quote', type: 'text' },
+    { name: 'result', type: 'string' }, // "3x leads", "50% more traffic"
+  ],
+}
